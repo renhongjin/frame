331ef8b325fd4c5e16c3f44b2428f9030963261c
ren hongjin <renhongjin@danlu.com>
2017-04-24 09:15:30 +0800

    新增httpclient

---
+++ src/com/photo/utils/HttpUtils.java
@@ -0,0 +1,33 @@
+package com.photo.utils;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class HttpUtils {
+    private static Log log = LogFactory.getLog(HttpUtils.class);
+    
+    public static String get(String url){
+        // 根据地址获取请求
+        HttpGet request = new HttpGet(url);//这里发送get请求
+        // 获取当前客户端对象
+        HttpClient httpClient = new DefaultHttpClient();    
+        // 通过请求对象获取响应对象
+        HttpResponse response;
+        try {
+            response = httpClient.execute(request);
+            // 判断网络连接状态码是否正常(0--200都数正常)
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                return EntityUtils.toString(response.getEntity(),"utf-8");
+            }
+        } catch (Exception e) {
+            log.error("向solr发送get请求异常"+url,e);
+        }
+        return null;
+    }
+}
--- src/com/photo/web/common/IndexController.java
+++ src/com/photo/web/common/IndexController.java
@@ -1,11 +1,17 @@
 package com.photo.web.common;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.photo.utils.HttpUtils;
 import com.photo.utils.NetworkUtil;
 
 @Controller
@@ -19,7 +25,19 @@ public class IndexController {
     public ModelAndView index(HttpServletRequest request){
         ModelAndView mv = new ModelAndView("index");
         String clientIp = NetworkUtil.getIpAddress(request);
-        mv.addObject("clientIp", clientIp);
+        String uri = "http://ip.taobao.com/service/getIpInfo.php?ip="+clientIp;
+        String jsonStr = HttpUtils.get(uri);
+        JSONObject data = JSON.parseObject(jsonStr);
+        Map<String,String> area = new HashMap<String,String>();
+        if(data.getIntValue("code") != 1){
+            area.put("country", data.getJSONObject("data").getString("country"));
+            area.put("region", data.getJSONObject("data").getString("region"));
+            area.put("city", data.getJSONObject("data").getString("city"));
+            area.put("isp", data.getJSONObject("data").getString("isp"));
+        }else{
+            area.put("country", "not exist internet.");
+        }
+        mv.addObject("area", area);
         return mv;
     }
 
